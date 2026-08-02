a2c5d7d44944fb22a8227404d4ca3053df631493
apeironshifting <apeironshifting@gmail.com>
2026-08-02 11:21:56 -0400

    Update client.ts

--- Downloads/shiftspace/src/lib/supabase/client.ts
+++ Downloads/shiftspace/src/lib/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr';
 import type { Database } from './types';
 
-const supabaseUrl = https:ayxqdqqjaokammzjtzat.supabase.co;
-const supabaseAnonKey = sb_publishable_PNwLJfSA7xHWeOs2jHu4MQ_euTIGjoX;
+const supabaseUrl = 'https:ayxqdqqjaokammzjtzat.supabase.co';
+const supabaseAnonKey = 'sb_publishable_PNwLJfSA7xHWeOs2jHu4MQ_euTIGjoX';
 
 export const supabase = createBrowserClient<Database>(supabaseUrl1, supabaseAnonKey);
