81bef558b6429673cc7f260ecadc8121da146216
apeironshifting <apeironshifting@gmail.com>
2026-08-09 16:37:40 -0400

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
-import { textToHex } from "lucide-react" // if lucide is used, otherwise omit
+import { textToHex } from "lucide-react" 
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
