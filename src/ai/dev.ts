4d4656ef2d79b5fa584d817b0bf90d4f299a8327
apeironshifting <apeironshifting@gmail.com>
2026-08-09 15:49:10 -0400

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/summarize-script-definitions.ts';
-import '@/ai/flows/embed-spotify-playlist.ts';
-import '@/ai/flows/generate-shift-script.ts';
