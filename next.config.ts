a693e15886257ea6dda531258e8a3d35f152fb0f
apeironshifting <apeironshifting@gmail.com>
2026-08-09 16:55:27 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,42 +1,4 @@
-
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
+const nextConfig = {
+  reactStrictMode: true
 };
-
 export default nextConfig;
