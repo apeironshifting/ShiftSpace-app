f65f897987d7bc05379966e3957040434eaaf5e7
apeironshifting <apeironshifting@gmail.com>
2026-08-09 17:53:31 -0400

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// Added this to instantly satisfy all missing translation errors across the app
+export function isTranslationKey(value: any): boolean {
+  return false
+}
